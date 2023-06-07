d530f892146a0f05091f6f76115a691806aca343
rlDiversion <dylan.vn@live.be>
2023-06-07 13:19:28 +0200

    fully implemented brasserie button

--- plugin/scripts/brasserie.js
+++ plugin/scripts/brasserie.js
@@ -8,8 +8,12 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 });
 
 $(document).ready(function () {
-    chrome.storage.local.get({ inputData: {} }, function (data) {
-        var techid = data.inputData.techID;
-        document.getElementById("techid").value = techid;
+    chrome.storage.local.get({ brasserie: false }, function (settings) {
+        if (settings.brasserie) {
+            chrome.storage.local.get({ inputData: {} }, function (data) {
+                var techid = data.inputData.techID;
+                document.getElementById("techid").value = techid;
+            });
+        }
     });
 });
